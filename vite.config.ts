256cbb15e790f95239d55f76ad373d6a16a56ee7
Tomas Vondracek <tom@Tomass-MacBook-Pro.local>
2025-09-23 09:33:59 +0200

    no external config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    // Uncomment this to make it work
+    // ssr: {
+    //     noExternal: ['headless-tooltip'],
+    // }
 });
